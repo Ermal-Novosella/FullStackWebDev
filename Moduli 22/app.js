839e2f827825b9fcb68e337b452738fac03fccc2
Ermal-Novosella <ermalnovosella007@gmail.com>
2024-08-20 18:05:34 +0200

    Moduli 22

---
+++ Moduli 22/app.js
@@ -0,0 +1,73 @@
+console.log("HELLOOOOOOO");
+var x = 5;
+var y = 7;
+console.log(x+y);
+var b = 10;
+var c = 20;
+var d = b+c;
+console.log(d);
+
+var k = 10;
+var l = 4;
+var n = k%l;
+console.log(n);
+
+var m = 99
+m++;
+console.log(m);
+
+var o =101;
+o--;
+console.log(o);
+
+var p = 5;
+var q = 2;
+var s = p**q;
+
+
+var e = 15
+e+=5;
+console.log(e);
+
+e-=15
+console.log(e);
+
+var u = 10;
+u*=3;
+console.log(u);
+
+u/=3;
+console.log(u);
+
+
+
+var li = 5
+var ka = 5
+console.log(li == ka)
+
+var nj = 8
+console.log(nj != ka);
+
+
+console.log(nj > ka);
+
+console.log(nj < ka);
+
+var nr = 123;
+var tekst = "123";
+console.log(nr == tekst);
+
+var emriIpare = "Digital";
+var mbiemri = "School"
+var fullname = emriIpare + " " + mbiemri;
+console.log(fullname);
+
+
+console.log(`Hello from ${fullname}`)
+
+
+var age = 24;
+console.log(age >= 18 && age<=65)
+console.log(age===5 || age===25)
+
+console.log(!(age===4));
