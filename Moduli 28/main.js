d46be3a5a6987749479e45f31ce1f65c9b6c39c6
Ermal-Novosella <ermalnovosella007@gmail.com>
2024-08-29 16:20:31 +0200

    Moduli 28

---
+++ Moduli 28/main.js
@@ -0,0 +1,25 @@
+
+//do while loops
+var i = 0
+
+do{
+    i++
+    console.log(i);
+} while(i<5)
+
+
+//while loops
+var j = 0;
+
+while(j<0){
+    j++
+    console.log(j);
+}
+
+// for loops
+
+for(var k=0; k<6;k++){
+    console.log("k-ja eshte "+ k);
+}
+
+//
