4f1794e34242d714fff5fadea79e42c2356ce7ae
Ermal-Novosella <ermalnovosella007@gmail.com>
2024-09-05 18:30:29 +0200

    Moduli 34

---
+++ Moduli 34/main.js
@@ -0,0 +1,14 @@
+$(window).ready(function(){
+    alert("Its working");
+});
+
+$("#header");
+$("li");
+$("ul li");
+$(".student");
+$("a.test:first");
+$("tr.odd");
+
+$("li").click(function(){
+    alert("Clicked!!!!!!!!!!!!");
+})
