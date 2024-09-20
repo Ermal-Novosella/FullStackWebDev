3a60efa101527f16113e5c580d08d28c19ce5422
Ermal-Novosella <ermalnovosella007@gmail.com>
2024-09-20 18:46:25 +0200

    Create moduli1.php

---
+++ moduli1.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+    // echo "ermal";
+    // $school ="Naim Frasheri";
+    // echo"i love $school";
+    // $x = 130;
+    // $y = 72;
+    // echo $x + $y;   
+    // echo $x - $y;   
+    // echo $x * $y;   
+    // echo $x / $y;   
+    // $a ="Sami";
+    // $b ="Frasheri";
+    // $c = $a.=$b;
+    // echo "$c \n";
+
+    // $ermal = "Ermali";
+    // echo strlen($ermal);
+    // echo str_word_count($ermal);
+
+    // $programmer = "Programmimg is not cool";
+    // echo str_replace("not","very", $programmer);
+
+    $EMRI = "ermal";
+    echo strrev($EMRI);
+
+
+?>
+</body>
+</html>
